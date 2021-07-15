1b574e9de11edf850249d8d8841470a5403a16e8
ShivaOblivion <65827397+ShivaOblivion@users.noreply.github.com>
2021-07-15 07:09:48 -0400

    mouv

--- Assets/test_mouv.cs
+++ Assets/test_mouv.cs
@@ -6,18 +6,26 @@ public class test_mouv : MonoBehaviour
     public float moveSpeed;
     private Vector3 velocity = Vector3.zero;
     public Rigidbody2D rb;
+    public float jumpForce;
+    private float TimeSinceJump = 6.0f;
+    
+    
+    
+    
+    
+    void start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    
     
     // calcul du mouve
-    void fixUpdate()
+    void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        
-        MovePlayer(horizontalMovement);
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
+        rb.velocity = new Vector2(horizontalMovement, 0);
     }
-
-    void MovePlayer(float _horizontalMovement)
-    {
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
-    }
+    
 }
+  
