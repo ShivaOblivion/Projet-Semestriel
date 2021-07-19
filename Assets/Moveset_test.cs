2cf79407d69212d68fc65584e658cfefa1beb45a
EzechielAMIGORENA <6-15820@saeinstitute.edu>
2021-07-19 16:16:56 +0200

    lundi fin de cours

--- Assets/Moveset_test.cs
+++ Assets/Moveset_test.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class test_mouv : MonoBehaviour
+public class Moveset_test : MonoBehaviour
 {
     public float moveSpeed;
     private Vector3 velocity = Vector3.zero;
@@ -9,35 +11,44 @@ public class test_mouv : MonoBehaviour
     public float jumpForce;
     private float TimeSinceJump = 6.0f;
     public bool isJumping = false;
-    
-    
-    
-    
+    private float gravity;
+
+
+
     void start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
     }
-    
-    
+
+
     // calcul du mouve
     void Update()
     {
+        isJumping = false;
+
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
         rb.velocity = new Vector2(horizontalMovement, 0);
-
-
-        if (Input.GetButtonDown("Jump"))
+        if (!isJumping)
         {
-            isJumping = true;
+            gravity += 0.3f;
+            gravity = Mathf.Clamp(gravity, 0f, 150f);
+            rb.AddForce(new Vector2(0f, -gravity));
         }
 
-        
+        Debug.Log(gravity);
+        if (Input.GetButtonDown("Jump"))
+        {
+            {
+                gravity = 0;
+                rb.AddForce(new Vector2(0f, jumpForce));
+                Debug.Log(1);
+                isJumping = true;
+            }
+        }
+
+
     }
 
-    void Jump()
-    {
-        rb.AddForce(new Vector2(0f, jumpForce));
-        isJumping = false;
-    }
+
 }
