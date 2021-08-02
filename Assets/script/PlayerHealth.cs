db212fc1b82c48e919b7226ac4e8331efb0aeb74
ShivaOblivion <65827397+ShivaOblivion@users.noreply.github.com>
2021-08-02 05:37:55 -0400

    CS
    cs

---
+++ Assets/script/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+    public void update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            takeDamage(20);
+        }     
+    }
+    public void takeDamage(int damage)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+        }      
+}   
+    
+   
---
+++ Assets/script/Projectile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject tower;
+    public GameObject target;
+    public float speed = 10f;
+    private float towerX;
+    private float targetX;
+    private float dist;
+    private float nextX;
+    private float baseY;
+    private float height;
+    
+    void Start()
+    {
+        tower = GameObject.FindGameObjectWithTag("Tower");
+        target = GameObject.FindGameObjectWithTag("Enemy");
+    }
+    void Update()
+    {
+        towerX = tower.transform.position.x;
+        targetX = target.transform.position.x;
+
+        dist = targetX - towerX;
+        nextX = Mathf.MoveTowards(transform.position.x, targetX, speed * Time.deltaTime);
+        baseY = Mathf.Lerp(tower.transform.position.y, target.transform.position.y, (nextX - towerX) / dist);
+
+        Vector3 movePosition = new Vector3(nextX, baseY + height, transform.position.z);
+        transform.rotation = LookAtTarget(movePosition - transform.position);
+        transform.position = movePosition;
+
+        if (transform.position == target.transform.position)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static Quaternion LookAtTarget(Vector2 rotation)
+    {
+        return Quaternion.Euler(0, 0, Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg);
+    }
+}
